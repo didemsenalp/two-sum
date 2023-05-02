3da167893ec29749f77bfb0fe00ed14e9f93ce7a
Didem Şenalp <didemsenalp8@gmail.com>
2023-05-02 15:15:25 +0300

    son hali

--- two-sum.py
+++ two-sum.py
@@ -1,13 +1,14 @@
 def twoSum(nums, target):
-    if (2 <= len(nums) <= 104 and -109 <= target <= 109 and -109 <= nums[i] <= 109):
+    if (2 <= len(nums) <= (10 ** 4) and (-10 ** 9) <= target <= (10 ** 9)):
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                print(i,j)
-                print(int(nums[i]),int(nums[j]),target)
-                print(int(nums[i]) + int(nums[j]) == target,i != j )
-                if (i != j and int(nums[i]) + int(nums[j]) == target):
+            if((-10 ** 9) <= int(nums[i]) <= (10 ** 9)):
+                for j in range(i + 1, len(nums)):
                     print(i,j)
-                    return (i, j)
+                    print(int(nums[i]),int(nums[j]),target)
+                    print(int(nums[i]) + int(nums[j]) == target,i != j )
+                    if (i != j and int(nums[i]) + int(nums[j]) == target):
+                        print(i,j)
+        return (i, j)
 
 
 
